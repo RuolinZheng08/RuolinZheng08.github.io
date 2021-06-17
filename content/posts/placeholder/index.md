341af675f8a8550e5f1f5b38d66148c975f52e78
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-06-17 16:29:12 -0700

    Start refactoring my site using Brittany Chang's template

---
+++ content/posts/placeholder/index.md
@@ -0,0 +1,11 @@
+---
+title: placeholder
+description: placeholder
+date: 2021-06-17
+draft: true
+slug: /pensieve/placeholder
+tags:
+  - Placeholder
+---
+
+Placeholder
---
+++ src/components/sections/hero.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import styled from 'styled-components';
+import { email } from '@config';
+import { navDelay, loaderDelay } from '@utils';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledHeroSection = styled.section`
+  ${({ theme }) => theme.mixins.flexCenter};
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  padding: 0;
+
+  @media (max-width: 480px) and (min-height: 700px) {
+    padding-bottom: 10vh;
+  }
+
+  h1 {
+    margin: 0 0 30px 4px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+    font-weight: 400;
+
+    @media (max-width: 480px) {
+      margin: 0 0 20px 2px;
+    }
+  }
+
+  h3 {
+    margin-top: 10px;
+    color: var(--slate);
+    line-height: 0.9;
+  }
+
+  p {
+    margin: 20px 0 0;
+    max-width: 500px;
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const Hero = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    const timeout = setTimeout(() => setIsMounted(true), navDelay);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const one = <h1>Hi, my name is</h1>;
+  const two = <h2 className="big-heading">Lynn Zheng.</h2>;
+  const three = <h3 className="big-heading">I create software and games.</h3>;
+  const four = (
+    <p>
+      I'm a Boston-based software engineer who specializes in building (and occasionally designing)
+      exceptional digital experiences. Currently, I'm an engineer at{' '}
+      <a href="https://upstatement.com/">Upstatement</a> focused on building accessible,
+      human-centered products.
+    </p>
+  );
+  const five = (
+    <a href={`mailto:${email}`} className="email-link">
+      Get In Touch
+    </a>
+  );
+
+  const items = [one, two, three, four, five];
+
+  return (
+    <StyledHeroSection>
+      {prefersReducedMotion ? (
+        <>
+          {items.map((item, i) => (
+            <div key={i}>{item}</div>
+          ))}
+        </>
+      ) : (
+        <TransitionGroup component={null}>
+          {isMounted &&
+            items.map((item, i) => (
+              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
+                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
+              </CSSTransition>
+            ))}
+        </TransitionGroup>
+      )}
+    </StyledHeroSection>
+  );
+};
+
+export default Hero;
