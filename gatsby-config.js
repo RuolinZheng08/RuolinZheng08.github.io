059c85c70221a2a1a44d657732d59f598a218ed2
Lynn Zheng <ruolinzheng@uchicago.edu>
2021-12-22 15:59:41 -0800

    Try to fix Twitter card

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Lynn Zheng is a software engineer, hobbyist game developer, and open-source enthusiast.',
     siteUrl: 'https://ruolinzheng08.github.io', // No trailing slash allowed!
-    image: 'og.png', // Path to your image you placed in the 'static' folder
+    image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: 'lynnzheng08',
   },
   plugins: [
